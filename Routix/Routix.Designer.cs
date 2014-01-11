8facf60d47ca0deb484814d8ef60140d6e6d94e4
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2014-01-11 19:58:31 +0100

    dodano obsługę podstawowej komunikacji z LRM-ami. jest budowa grafu sieci

---
+++ Routix/Routix.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Routix
+{
+    partial class Routix
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.conToCloudButton = new System.Windows.Forms.Button();
+            this.cloudIPTextBox = new System.Windows.Forms.TextBox();
+            this.conLabel = new System.Windows.Forms.Label();
+            this.log = new System.Windows.Forms.TextBox();
+            this.cloudPortTextBox = new System.Windows.Forms.TextBox();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.networkNumerLabel = new System.Windows.Forms.Label();
+            this.subnetNumberLabel = new System.Windows.Forms.Label();
+            this.networkNumberTextBox = new System.Windows.Forms.TextBox();
+            this.subnetTextBox = new System.Windows.Forms.TextBox();
+            this.sendTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // conToCloudButton
+            // 
+            this.conToCloudButton.Location = new System.Drawing.Point(9, 140);
+            this.conToCloudButton.Name = "conToCloudButton";
+            this.conToCloudButton.Size = new System.Drawing.Size(117, 32);
+            this.conToCloudButton.TabIndex = 0;
+            this.conToCloudButton.Text = "Połącz";
+            this.conToCloudButton.UseVisualStyleBackColor = true;
+            this.conToCloudButton.Click += new System.EventHandler(this.conToCloudButton_Click);
+            // 
+            // cloudIPTextBox
+            // 
+            this.cloudIPTextBox.Location = new System.Drawing.Point(9, 75);
+            this.cloudIPTextBox.Name = "cloudIPTextBox";
+            this.cloudIPTextBox.Size = new System.Drawing.Size(117, 20);
+            this.cloudIPTextBox.TabIndex = 1;
+            this.cloudIPTextBox.Text = "127.0.0.1";
+            // 
+            // conLabel
+            // 
+            this.conLabel.AutoSize = true;
+            this.conLabel.Location = new System.Drawing.Point(9, 56);
+            this.conLabel.Name = "conLabel";
+            this.conLabel.Size = new System.Drawing.Size(108, 13);
+            this.conLabel.TabIndex = 2;
+            this.conLabel.Text = "IP chmury sterowania";
+            // 
+            // log
+            // 
+            this.log.BackColor = System.Drawing.SystemColors.Window;
+            this.log.Location = new System.Drawing.Point(7, 178);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.log.Size = new System.Drawing.Size(426, 153);
+            this.log.TabIndex = 13;
+            // 
+            // cloudPortTextBox
+            // 
+            this.cloudPortTextBox.Location = new System.Drawing.Point(9, 114);
+            this.cloudPortTextBox.Name = "cloudPortTextBox";
+            this.cloudPortTextBox.Size = new System.Drawing.Size(117, 20);
+            this.cloudPortTextBox.TabIndex = 24;
+            this.cloudPortTextBox.Text = "13000";
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(9, 98);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(117, 13);
+            this.portLabel.TabIndex = 23;
+            this.portLabel.Text = "Port chmury sterowania";
+            // 
+            // networkNumerLabel
+            // 
+            this.networkNumerLabel.AutoSize = true;
+            this.networkNumerLabel.Location = new System.Drawing.Point(9, 9);
+            this.networkNumerLabel.Name = "networkNumerLabel";
+            this.networkNumerLabel.Size = new System.Drawing.Size(62, 13);
+            this.networkNumerLabel.TabIndex = 25;
+            this.networkNumerLabel.Text = "Numer sieci";
+            // 
+            // subnetNumberLabel
+            // 
+            this.subnetNumberLabel.AutoSize = true;
+            this.subnetNumberLabel.Location = new System.Drawing.Point(9, 36);
+            this.subnetNumberLabel.Name = "subnetNumberLabel";
+            this.subnetNumberLabel.Size = new System.Drawing.Size(80, 13);
+            this.subnetNumberLabel.TabIndex = 26;
+            this.subnetNumberLabel.Text = "Numer podsieci";
+            // 
+            // networkNumberTextBox
+            // 
+            this.networkNumberTextBox.Location = new System.Drawing.Point(95, 9);
+            this.networkNumberTextBox.Name = "networkNumberTextBox";
+            this.networkNumberTextBox.Size = new System.Drawing.Size(31, 20);
+            this.networkNumberTextBox.TabIndex = 27;
+            // 
+            // subnetTextBox
+            // 
+            this.subnetTextBox.Location = new System.Drawing.Point(95, 33);
+            this.subnetTextBox.Name = "subnetTextBox";
+            this.subnetTextBox.Size = new System.Drawing.Size(31, 20);
+            this.subnetTextBox.TabIndex = 28;
+            // 
+            // sendTextBox
+            // 
+            this.sendTextBox.Location = new System.Drawing.Point(7, 338);
+            this.sendTextBox.Name = "sendTextBox";
+            this.sendTextBox.Size = new System.Drawing.Size(344, 20);
+            this.sendTextBox.TabIndex = 29;
+            this.sendTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sendTextBox_KeyPress);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Enabled = false;
+            this.sendButton.Location = new System.Drawing.Point(358, 335);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 30;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // Routix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 365);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.sendTextBox);
+            this.Controls.Add(this.subnetTextBox);
+            this.Controls.Add(this.networkNumberTextBox);
+            this.Controls.Add(this.subnetNumberLabel);
+            this.Controls.Add(this.networkNumerLabel);
+            this.Controls.Add(this.cloudPortTextBox);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.conLabel);
+            this.Controls.Add(this.cloudIPTextBox);
+            this.Controls.Add(this.conToCloudButton);
+            this.Name = "Routix";
+            this.Text = "Routix";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button conToCloudButton;
+        private System.Windows.Forms.TextBox cloudIPTextBox;
+        private System.Windows.Forms.Label conLabel;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.TextBox cloudPortTextBox;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.Label networkNumerLabel;
+        private System.Windows.Forms.Label subnetNumberLabel;
+        private System.Windows.Forms.TextBox networkNumberTextBox;
+        private System.Windows.Forms.TextBox subnetTextBox;
+        private System.Windows.Forms.TextBox sendTextBox;
+        private System.Windows.Forms.Button sendButton;
+
+    }
+}
---
+++ Routix/Routix.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuickGraph;
+using AddressLibrary;
+using System.IO;
+
+namespace Routix {
+
+    public partial class Routix : Form {
+
+        delegate void SetTextCallback(string text);
+
+        private const bool isDebug = true;
+
+        private Address myAddr;
+
+        //dane chmury
+        private IPAddress cloudAddress;        //Adres na którym chmura nasłuchuje
+        private Int32 cloudPort;           //port chmury
+        private IPEndPoint cloudEndPoint;
+        private Socket cloudSocket;
+
+        private Thread receiveThread;     //wątek służący do odbierania połączeń
+        private Thread sendThread;        // analogicznie - do wysyłania
+
+        private Queue _whatToSendQueue;
+        private Queue whatToSendQueue;
+
+        //strumienie
+        private NetworkStream networkStream;
+
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        public bool isConnectedToCloud { get; private set; } // czy połączony z chmurą?
+
+        //biblioteka z tymi grafami jest chujowa i nie da się przypisać słownikowi w niej zawartemu odpowiedniego comparera
+        //dlatego graf będzie zawierać adresy w postaci odpowiadających im stringów :<
+        private AdjacencyGraph<String, Edge<String>> networkGraph;
+        /// <summary>
+        /// konstruktor
+        /// </summary>
+        public Routix() {
+            isConnectedToCloud = false;
+            InitializeComponent();
+            networkGraph = new AdjacencyGraph<String, Edge<String>>();
+            _whatToSendQueue = new Queue();
+            //synchroniczny wrapper dla kolejki
+            whatToSendQueue = Queue.Synchronized(_whatToSendQueue);
+        }
+        /// <summary>
+        /// metoda wywołana po wciśnięciu "połącz z chmurą"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void conToCloudButton_Click(object sender, EventArgs e) {
+            if (!isConnectedToCloud) {
+                if (setAddress()) {
+                    if (IPAddress.TryParse(cloudIPTextBox.Text, out cloudAddress)) {
+                        SetText("IP ustawiono jako " + cloudAddress.ToString());
+                    } else {
+                        SetText("Błąd podczas ustawiania IP chmury (zły format?)");
+                    }
+                    if (Int32.TryParse(cloudPortTextBox.Text, out cloudPort)) {
+                        SetText("Port chmury ustawiony jako " + cloudPort.ToString());
+                    } else {
+                        SetText("Błąd podczas ustawiania portu chmury (zły format?)");
+                    }
+
+                    cloudSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    cloudEndPoint = new IPEndPoint(cloudAddress, cloudPort);
+                    try {
+                        cloudSocket.Connect(cloudEndPoint);
+                        isConnectedToCloud = true;
+                        networkStream = new NetworkStream(cloudSocket);
+                        writer = new StreamWriter(networkStream);
+                        reader = new StreamReader(networkStream);
+                        sendButton.Enabled = true;
+                        whatToSendQueue.Enqueue("HELLO " + myAddr);
+                        receiveThread = new Thread(this.receiver);
+                        receiveThread.IsBackground = true;
+                        receiveThread.Start();
+                        sendThread = new Thread(this.sender);
+                        sendThread.IsBackground = true;
+                        sendThread.Start();
+                        conToCloudButton.Text = "Rozłącz";
+                        SetText("Połączono!");
+                    } catch (SocketException) {
+                        isConnectedToCloud = false;
+                        SetText("Błąd podczas łączenia się z chmurą");
+                        SetText("Złe IP lub port? Chmura nie działa?");
+                    }
+                } else {
+                    SetText("Wprowadź numery sieci i podsieci");
+                }
+            } else {
+                isConnectedToCloud = false;
+                sendButton.Enabled = false;
+                conToCloudButton.Text = "Połącz";
+                SetText("Rozłączono!");
+                if (cloudSocket != null) cloudSocket.Close();
+            }
+        }
+
+        #region threads
+        /// <summary>
+        /// wątek odbierający wiadomości z chmury
+        /// </summary>
+        public void receiver() {
+            String _msg;
+            while (isConnectedToCloud) {
+                _msg = reader.ReadLine();
+                if (isDebug) SetText("Odczytano: " + _msg);
+                String[] _msgArray = _msg.Split(new char[] {':'}, 2);
+                Address _senderAddr;
+                if (Address.TryParse(_msgArray[0], out _senderAddr)) {
+                    if (_senderAddr.host == 0) {
+                    #region FROM ANOTHER RC
+
+                    #endregion
+                    }
+                    else if (_senderAddr.host == 1) {
+                    #region FROM CC
+                        
+                    #endregion
+                    }
+                    else {
+                    #region FROM LRM
+                        String[] _LRMmsg = _msgArray[1].Split(new char[] { ' ' });
+                        //gdy logowanie się LRM
+                        if (_LRMmsg[0] == "HELLO") {
+                            Address _addr;
+                            if (Address.TryParse(_LRMmsg[1], out _addr)) {
+                                if (networkGraph.ContainsVertex(_addr.ToString())) {
+                                    whatToSendQueue.Enqueue(_senderAddr.ToString() + ":ADDR_TAKEN");
+                                } else {
+                                    networkGraph.AddVertex(_addr.ToString());
+                                    if (isDebug) SetText("Dodano węzeł grafu");
+                                    whatToSendQueue.Enqueue(_addr.ToString() + ":REQ_TOPOLOGY");
+                                }
+                            }
+                        }
+                        if (_LRMmsg[0] == "TOPOLOGY") {
+                            String[] _neighbors = new String[_LRMmsg.Length - 1];
+                            for (int i = 1; i < _LRMmsg.Length; i++) {
+                                _neighbors[i - 1] = _LRMmsg[i];
+                            }
+                            foreach (String str in _neighbors) {
+                                Address _destAddr;
+                                if (Address.TryParse(str, out _destAddr)) {
+                                    networkGraph.AddEdge(new Edge<String>(_senderAddr.ToString(), _destAddr.ToString()));
+                                    if (isDebug) SetText("Dodano ścieżkę z " + _senderAddr.ToString() + "do " + _destAddr.ToString());
+                                }
+                            }
+                        }
+                    #endregion
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// wątek wysyłający wiadomości do chmury
+        /// </summary>
+        public void sender() {
+            while(isConnectedToCloud) {
+                if (whatToSendQueue.Count != 0) {
+                    String _msg = (String)whatToSendQueue.Dequeue();
+                    writer.WriteLine(_msg);
+                    writer.Flush();
+                    if (isDebug) SetText("Wysłano: " + _msg);
+                }
+            }    
+        }
+        #endregion
+
+        #region logging & address setup
+        /// <summary>
+        /// metoa ustalająca adres RC
+        /// </summary>
+        /// <returns>czy się udało czy nie</returns>
+        public bool setAddress() {
+            int _netNum;
+            int _subnetNum;
+            if (int.TryParse(networkNumberTextBox.Text, out _netNum))
+                if (int.TryParse(subnetTextBox.Text, out _subnetNum)) {
+                     myAddr = new Address(_netNum, _subnetNum, 0);
+                    return true;
+                }
+                else return false;
+            else return false;
+        }
+        /// <summary>
+        /// wstawienie tekstu do logu
+        /// </summary>
+        /// <param name="text">tekst, który metoda ma wstawić</param>
+        public void SetText(string text) {
+            // InvokeRequired required compares the thread ID of the 
+            // calling thread to the thread ID of the creating thread. 
+            // If these threads are different, it returns true. 
+            if (this.log.InvokeRequired) {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            } else {
+                try {
+                    this.log.AppendText(text+"\n");
+                } catch { }
+            }
+        }
+        #endregion
+
+        #region sending messages
+        /// <summary>
+        /// metoda wywołana po wciśnięciu "wyślij"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sendButton_Click(object sender, EventArgs e) {
+            whatToSendQueue.Enqueue(sendTextBox.Text);
+            sendTextBox.Clear();
+        }
+        /// <summary>
+        /// obsługa wciśnięcia klawisza ENTER
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sendTextBox_KeyPress(object sender, KeyPressEventArgs e) {
+            if (sendButton.Enabled && e.KeyChar.Equals((char)Keys.Enter)) sendButton_Click(sender, e);
+        }
+        #endregion
+    }
+}
